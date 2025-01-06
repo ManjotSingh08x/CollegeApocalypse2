67ce7e46893ab1a40d7face92f260ac6ce38ab9b
Manjot Singh <mithugunnu2006@gmail.com>
2025-01-06 23:08:30 +0530

    loading and saving functionality complete
    now switching scenes saves the state of the chest inventory, stores the state of the pickup items and the zombie location

--- Scripts/DataManaging/save_level.gd
+++ Scripts/DataManaging/save_level.gd
@@ -4,3 +4,6 @@ class_name SaveLevel
 @export var level_name: String = "default"
 @export var zombie_dict: Dictionary = {}
 @export var chest_dict: Dictionary = {}
+
+# structure: {"name" : {"item_name", "global_position", "quantity", "coins"}}
+@export var pick_up_dict: Dictionary = {}
